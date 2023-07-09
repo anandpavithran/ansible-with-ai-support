d504050f0e2463914f0fa922171b5b25d5dba599
Student User <student@workstation.lab.example.com>
2023-07-09 10:51:58 +0000

    test

--- do374/init.sh
+++ do374/init.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+export ANSIBLE_CONFIG=/home/student/ansible-with-ai-support/do374/pre_tasks/ansible.cfg
+ansible --version
 ansible-playbook -i /home/student/ansible-with-ai-support/do374/pre_tasks/inventory /home/student/ansible-with-ai-support/do374/pre_tasks/main1.yml
+unset ANSIBLE_CONFIG
