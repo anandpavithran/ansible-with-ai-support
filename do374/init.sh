9a14747f530e35203516ec02acf5c5e65843c4aa
anandpavithran <58514903+anandpavithran@users.noreply.github.com>
2023-07-09 16:07:44 +0530

    Update init.sh

--- do374/init.sh
+++ do374/init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -i /home/anandpavithran/ansible-with-ai-support/do374/pre_tasks/inventory /home/anandpavithran/ansible-with-ai-support/do374/pre_tasks/main1.yml
+ansible-playbook -i /home/student/ansible-with-ai-support/do374/pre_tasks/inventory /home/student/ansible-with-ai-support/do374/pre_tasks/main1.yml
